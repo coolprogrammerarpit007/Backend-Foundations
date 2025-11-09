a1a570600c0431d8260a55ab8382a746ac24b990
microprixs <arpit.mishra@microprixs.in>
2025-11-09 19:39:03 +0530

    Python Backend Foundations

---
+++ main.py
@@ -0,0 +1,24 @@
+# print("Hello World!")
+# message = 'Nice to meet you!'
+# # print(message)
+
+# greeting = 'Hello'
+# name = 'Arpit'
+
+# updated_msg = '{}, {}. {}'.format(greeting,name,message)
+# # print(updated_msg)
+
+
+# print(dir(name)) -> returns the method and attributes associated with obj
+# help(str)
+
+# help(str)
+
+
+courses = ['Math','English','Science','Social Science']
+
+for index,course in enumerate(courses):
+    print(f"Course Number {index+1}: {course}")
+
+course_str = ','.join(courses)
+print(course_str)
