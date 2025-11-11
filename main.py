71140a1cef0c19a4ee46282e8a2e83c8fded31f4
microprixs <arpit.mishra@microprixs.in>
2025-11-11 11:29:18 +0530

    Scoping In  Python LEGB Rule

--- main.py
+++ main.py
@@ -15,13 +15,13 @@
 # help(str)
 
 
-courses = ['Math','English','Science','Social Science']
+# courses = ['Math','English','Science','Social Science']
 
-for index,course in enumerate(courses):
-    print(f"Course Number {index+1}: {course}")
+# for index,course in enumerate(courses):
+#     print(f"Course Number {index+1}: {course}")
 
-course_str = ','.join(courses)
-print(course_str)
+# course_str = ','.join(courses)
+# print(course_str)
 
 
 
@@ -30,7 +30,60 @@ print(course_str)
 # None
 # Zero of Any Numerical Type
 # Any Empty Sequence -> [],(),{},''
+
 # Any Empty Mapping -> {}
 
 
 # An In-depth look at pip -> python package management system
+
+
+
+
+# for i in range(1,6):
+#     print(str(i)*i)
+
+
+# Scoping In Python
+
+# '''
+# # LEGB -> Local,Enclosing,Global.Built-In
+# Python first checks in local then enclosing scope then Into Global Scope then Into Built-In Scope
+# '''
+
+# x = 'Global X'
+
+def test():
+    global x
+    x = 'Local X'
+    print(x)
+    
+    
+# test()
+# print(x)
+
+
+# Enclosing Scope
+# x = 500
+def test1():
+    x = 100
+    def test2():
+        print(x)
+        
+    test2()
+    
+# test1()
+# print(x)
+
+
+def outer():
+    x = 'Outer X'
+    def inner():
+        # x = 'Inner X'
+        nonlocal x
+        x = 'Updated to Inner X'
+        print(x)
+        
+    inner()
+    print(x)
+    
+outer()
