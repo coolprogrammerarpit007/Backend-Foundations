b14b29948e42619eb35680fb6a09d83acd94ad17
microprixs <arpit.mishra@microprixs.in>
2025-11-11 12:45:30 +0530

    Learn about list,set and dict comprehensions and map,filter,zip and lampda function in python along with generators and generator expressions in python

--- main.py
+++ main.py
@@ -97,3 +97,75 @@ my_list = [0,1,2,3,4,5,6,7,8,9]
 
 # +ve step -> Means slicing direction is rightward from left to right
 # -ve step -> Means slicing direction is leftward from right to left -> reverse order
+
+# List,Set and Dictionary Comprehensions
+
+num_list = [number*number for number in my_list]
+# print("Number List!")
+# print(num_list)
+
+# using maps in python
+
+squared_number_list = list(map(lambda x:x*x,my_list))
+# print("Square Number List")
+# print(squared_number_list)
+
+even_num_list = [number for number in my_list if number % 2==0]
+# print(even_num_list)
+
+
+# Using Filter in Python
+
+filtered_even_numbers = list(filter(lambda x:x%2 ==0,my_list))
+# print(filtered_even_numbers)
+
+alphabet_list = [(letter,num) for letter in 'abcd' for num in range(4)]
+# print(alphabet_list)
+
+
+# dictionary Comprehensions
+
+names = ['Bruce','Clark','Peter','Logan','Wade']
+heros = ['Batman','Superman','SpiderMan','Wolverine','Deadpool']
+
+# zip function in python -> it takes to lists and convert it into list of tuples
+# print(list(zip(names,heros)))
+
+my_dict = {}
+for name,hero in zip(names,heros):
+    my_dict[name] = hero
+    
+    
+# print(my_dict)
+
+# dictionary_comprehensions
+
+my_hero_dict = {name:hero for name,hero in zip(names,heros) if name != 'Peter'}
+# print(my_hero_dict)
+
+
+# Set Comprehensions
+numbers = [100,100,200,200,300,300,400,400,500,500,600,600,700,700,800,800,900,900,1000,1000]
+my_set = {number for number in numbers}
+# print(my_set)    
+
+
+# Generator Expressions
+
+nums = [1,2,3,4,5,6,7,8,9,10]
+my_gen = (num*num for num in nums)
+# print(my_gen)
+
+for num in my_gen:
+    print(num)
+
+# generators 
+
+def my_generator():
+    for i in range(5):
+        yield i  # instead of return
+ 
+gen = my_generator()
+
+for value in gen:
+    print(value)
