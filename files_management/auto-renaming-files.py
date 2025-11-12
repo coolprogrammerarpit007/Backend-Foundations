5b36aa108d0c90e66778bffbaa26aa5d6a5948ab
Arpit <arpit.mishra@microprixs.in>
2025-11-12 19:49:54 +0530

    Auto File Renaming Script

--- files_management/auto-renaming-files.py
+++ files_management/auto-renaming-files.py
@@ -1,18 +1,48 @@
+# import os
+
+# # change directory
+# # Prefix the string with r to tell Python not to interpret backslashes:
+# os.chdir(r'C:\Users\Dell\Downloads\images_files_download')
+# # print(os.getcwd()) # -> to check current working directory
+
+# # to list everything in the directory
+
+# for l in enumerate(os.listdir()):
+#     # print(l)
+#     file_name,file_ext = os.path.splitext(l)
+#     # print(file_name,file_ext)
+#     # print(file_name.split(' '))
+#     file_title,file_number = file_name.split(' ')
+#     # print(file_title)
+#     new_name = f"{file_title}{file_ext}"
+#     os.rename(l,new_name)
+
+
 import os
 
+file_path = r'C:\Users\Dell\Downloads\images_files_download'
+
 # change directory
-# Prefix the string with r to tell Python not to interpret backslashes:
-os.chdir(r'C:\Users\Dell\Downloads\images_files_download')
-# print(os.getcwd()) # -> to check current working directory
 
-# to list everything in the directory
+os.chdir(file_path) # -> to get current working directory
 
-for l in enumerate(os.listdir()):
-    # print(l)
-    file_name,file_ext = os.path.splitext(l)
-    # print(file_name,file_ext)
-    # print(file_name.split(' '))
-    file_title,file_number = file_name.split(' ')
-    # print(file_title)
-    new_name = f"{file_title}{file_ext}"
-    os.rename(l,new_name)
+files = os.listdir() # -> list all modules and files in the path
+
+# sort files to rename it into consistent order
+files.sort()
+
+
+# loop through each file to rename
+
+for index,f in enumerate(files,start=1):
+    file_ext = os.path.splitext(f)[1] # -> get extension from the file
+    
+    if file_ext.lower() not in ['.jpg','.jpeg','.png','.webp']:
+        continue
+    
+    # Build New file Name
+    new_file_name = f"Walpaper-img-{index}{file_ext}"
+    
+    # rename file to new name
+    os.rename(f,new_file_name)
+
